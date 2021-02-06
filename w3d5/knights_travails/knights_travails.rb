2a0676ca531e57e15d032903c71a907850bd74e1
Justin Cheasty <cheasty@gmail.com>
2021-02-05 17:04:11 -0800

    Finished valid moves class method

--- w3d5/knights_travails/knights_travails.rb
+++ w3d5/knights_travails/knights_travails.rb
@@ -2,20 +2,17 @@ class KnightPathFinder
   def self.valid_moves(pos)
     row, col = pos
     possible_moves = {
-      up_up_right: [row+2,col+1],
-      up_right_right:[row+1,col+2],
-      down_right_right: [row-1,col+2],
-      down_down_right:
-      [row-2,col+1]
-      down_down_left:
-      [row-2,col-1]
-      down_left_left:
-      [row-1,col-2]
-      up_left_left:
-      [row+1,col-2]
-      up_up_left:
-      [row+2,col-1]
+      up_up_right:      [row + 2, col + 1],
+      up_right_right:   [row + 1, col + 2],
+      down_right_right: [row - 1, col + 2],
+      down_down_right:  [row - 2, col + 1],
+      down_down_left:   [row - 2, col - 1],
+      down_left_left:   [row - 1, col - 2],
+      up_left_left:     [row + 1, col - 2],
+      up_up_left:       [row + 2, col - 1]
     }
+  end
+  
   def initialize(position)
     @root_node = position
     @considered_positions = []
