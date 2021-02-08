052f0ce5ee23829dda93d996fa9f6876297f254c
jadin-press <jadinp@gmail.com>
2021-02-08 14:06:41 -0800

    Knights Travails 15 minute switch

--- w3d5/knights_travails/knights_travails.rb
+++ w3d5/knights_travails/knights_travails.rb
@@ -1,4 +1,6 @@
 class KnightPathFinder
+  attr_reader :grid, 
+  
   def self.valid_moves(pos)
     row, col = pos
     possible_moves = {
@@ -11,16 +13,26 @@ class KnightPathFinder
       up_left_left:     [row + 1, col - 2],
       up_up_left:       [row + 2, col - 1]
     }
+    possible_move_values = possible_moves.values
+    valid_moves = possible_move_values.select { |move| (0..7).include?(row) && (0..7).include?(col) }
   end
   
   def initialize(position)
     @root_node = position
-    @considered_positions = []
+    @considered_positions = [position]
+    @grid = Array.new(8) { Array.new(8) }
+  end
+
+  def new_move_positions(pos)
+    accepted_moves = KnightPathFinder.valid_moves(pos).reject { |move| @considered_positions.include?(move) }
+    @considered_positions += accepted_moves
   end
 
   def build_move_tree
-    
-  end
+    queue = [@root_node]
 
+    until 
+    queue << new_move_positions(queue.shift)
+  end
 
 end
