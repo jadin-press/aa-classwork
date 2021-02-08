3f85736e398439afb2d7e03e1e8729d568088690
Justin Cheasty <cheasty@gmail.com>
2021-02-08 15:01:59 -0800

    completed day 1

--- w3d5/knights_travails/knights_travails.rb
+++ w3d5/knights_travails/knights_travails.rb
@@ -1,6 +1,8 @@
+require_relative "../polytreenode/lib/00_tree_node.rb"
 class KnightPathFinder
-  attr_reader :grid, 
   
+  attr_reader :considered_positions
+
   def self.valid_moves(pos)
     row, col = pos
     possible_moves = {
@@ -14,25 +16,53 @@ class KnightPathFinder
       up_up_left:       [row + 2, col - 1]
     }
     possible_move_values = possible_moves.values
-    valid_moves = possible_move_values.select { |move| (0..7).include?(row) && (0..7).include?(col) }
+    
+    valid_moves = possible_move_values.select do |move| 
+      (0..7).include?(move[0]) && (0..7).include?(move[1])
+    end
   end
   
   def initialize(position)
-    @root_node = position
+    @root_node = PolyTreeNode.new(position)
     @considered_positions = [position]
-    @grid = Array.new(8) { Array.new(8) }
   end
 
+  # def new_move_positions(pos)
+  #   accepted_moves = KnightPathFinder.valid_moves(pos).reject { |move| @considered_positions.include?(move) }
+  #   @considered_positions += accepted_moves
+  #   accepted_moves
+  # end
   def new_move_positions(pos)
-    accepted_moves = KnightPathFinder.valid_moves(pos).reject { |move| @considered_positions.include?(move) }
-    @considered_positions += accepted_moves
+    accepted_moves = []
+    KnightPathFinder.valid_moves(pos).each do |move| 
+      unless @considered_positions.include?(move)
+        @considered_positions << move
+        accepted_moves << move
+      end
+    end
+    accepted_moves
   end
 
   def build_move_tree
     queue = [@root_node]
-
-    until 
-    queue << new_move_positions(queue.shift)
+    
+    until queue.empty?
+      current_node = queue.shift
+      new_move_positions(current_node.value).each do |position|
+        next_node = PolyTreeNode.new(position)
+        queue << next_node
+        current_node.add_child(next_node)
+      end
+    end
+    
   end
 
 end
+
+# k = KnightPathFinder.new([0, 0])
+
+# k.build_move_tree
+
+# p k.considered_positions.size
+
+# p KnightPathFinder.valid_moves([0,0])
