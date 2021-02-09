64f0c8e4b977f3bf054a840e93ec2b16ee168e24
jadin-press <jadinp@gmail.com>
2021-02-08 16:27:49 -0800

    TicTacToe 15 minute switch

--- w4d1/tic-tac-toe_ai/lib/tic_tac_toe_node.rb
+++ w4d1/tic-tac-toe_ai/lib/tic_tac_toe_node.rb
@@ -2,9 +2,13 @@ require_relative 'tic_tac_toe'
 
 class TicTacToeNode
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
+
   end
 
   def winning_node?(evaluator)
@@ -13,5 +17,12 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    @board.each_index do |i|
+      @board.each_index do |j|
+        if @board.empty?([i, j])
+          TicTacToeNode.new(@board.dup, @next_mover_mark, @board[i, j])
+        end
+      end
+    end
   end
 end
