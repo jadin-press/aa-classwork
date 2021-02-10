e93343190f7fc845dbc181f4eb2f53e933565349
Ali Ibsin <ibsin.a1996@gmail.com>
2021-02-09 16:18:34 -0800

    add rest of Board#move_piece, [], []=. print

--- w4d2/chess/board.rb
+++ w4d2/chess/board.rb
@@ -1,3 +1,5 @@
+require_relative 'piece.rb'
+
 class Board
   def initialize
     @rows = Array.new(8) { Array.new(8, nil) }
@@ -9,14 +11,48 @@ class Board
     end
   end
 
-  def move_piece(start, end)
-    if @rows[start[0]][start[1]].nil?
+  def move_piece(start, end_pos)
+    if self[start].nil?
       raise "There is no piece at this position"
     end
     size = (0..7).to_a
-    if !size.include?(end[0]) || !size.include?(end[1])
+    if !size.include?(end_pos[0]) || !size.include?(end_pos[1])
       raise "This piece cannot move to end position"
     end
-    
+
+    piece = self[start]
+    self[end_pos] = piece
+    self[start] = nil
   end
+
+  def [](position)
+    @rows[position[0]][position[1]]
+  end
+
+  def []=(position, value)
+    @rows[position[0]][position[1]] = value
+  end
+
+  def print
+    new_grid = @rows.to_a.map do |row|
+      row.to_a.map do |space|
+        if space.is_a?(Piece)
+          "P"
+        else
+          "N"
+        end
+      end
+    end
+
+    new_grid.each do |row|
+      puts row.join(" ")
+    end
+  end
+
 end
+
+
+# board = Board.new()
+# board.print
+# board.move_piece([1,3], [3,3])
+# board.print
