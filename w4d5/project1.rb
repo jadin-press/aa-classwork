c701d014f27cdef2fe1b37ae224ded3b5078cf42
jadin-press <jadinp@gmail.com>
2021-02-12 13:39:54 -0800

    Complete #largest_contiguous_subsum phase 1

--- w4d5/project1.rb
+++ w4d5/project1.rb
@@ -13,6 +13,36 @@ def my_min2(list)
 end
 #time complexity - O(n)
 
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 
-p my_min2(list)
+# p my_min2(list)
+
+def largest_contiguous_subsum(list)
+  subsets = []
+  list.each_with_index do |el1, i1| #n
+    list.each_with_index do |el2, i2| #n
+      if i2 >= i1
+        subsets << list[i1..i2]
+      end
+    end
+  end
+  max = subsets[0].sum
+  subsets.each do |arr|
+    sum = arr.sum
+    max = sum if sum > max
+  end
+  max
+end
+
+# time complexity == O(n^2 * nlog(n))
+
+# list = [-5, -1, -3]
+# p largest_contiguous_subsum(list)
+
+# [5, 6, 1, -3, -2, 5, 2, 4]
+# var_1 = 5 --> 11 --> 12 --> 7
+# var_2 = -3 --> -5
+
+def method_name
+  
+end
