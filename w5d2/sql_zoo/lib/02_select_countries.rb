826ea6b845a56721d8a43dfd35929696b254b25a
jadin-press <jadinp@gmail.com>
2021-02-16 16:26:47 -0800

    15 minute switch

--- w5d2/sql_zoo/lib/02_select_countries.rb
+++ w5d2/sql_zoo/lib/02_select_countries.rb
@@ -76,5 +76,11 @@ end
 def united_we_stand
   # Show the countries that have a name that includes the word 'United'
   execute(<<-SQL)
+    SELECT
+      countries.name
+    FROM
+      countries
+    WHERE
+      countries.name LIKE 'United%';
   SQL
 end
--- w5d2/sql_zoo/lib/03_select_nobel.rb
+++ w5d2/sql_zoo/lib/03_select_nobel.rb
@@ -24,24 +24,52 @@ end
 def prizes_from_1950
   # Display Nobel prizes for 1950.
   execute(<<-SQL)
+    SELECT
+      nobels.yr, nobels.subject, nobels.winner 
+    FROM
+      nobels
+    WHERE
+      nobels.yr = 1950;
   SQL
 end
 
 def literature_1962
   # Show who won the 1962 prize for Literature.
   execute(<<-SQL)
+    SELECT
+      nobels.winner
+    FROM
+      nobels
+    WHERE 
+      nobels.yr = 1962
+        AND
+      nobels.subject = 'Literature'
   SQL
 end
 
 def einstein_prize
   # Show the year and subject that won 'Albert Einstein' his prize.
   execute(<<-SQL)
+    SELECT
+      nobels.yr, nobels.subject
+    FROM
+      nobels
+    WHERE
+      nobels.winner = 'Albert Einstein';
   SQL
 end
 
 def millennial_peace_prizes
   # Give the name of the 'Peace' winners since the year 2000, including 2000.
   execute(<<-SQL)
+    SELECT
+      nobels.winner
+    FROM
+      nobels
+    WHERE
+      nobels.subject = 'Peace'  
+        AND
+      nobels.yr >= 2000
   SQL
 end
 
