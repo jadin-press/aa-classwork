72e98c274be35ce2256d246c7a5b1ed70439c2cd
skumtong <sky_kumtong@yahoo.com>
2021-02-17 14:25:18 -0800

    Modidy import_db.sql

--- w5d3/aa_questions/import_db.sql
+++ w5d3/aa_questions/import_db.sql
@@ -1,3 +1,9 @@
+DROP TABLE users;
+DROP TABLE questions;
+DROP TABLE question_follows;
+DROP TABLE replies;
+DROP TABLE question_likes; 
+
 PRAGMA foreign_keys = ON;
 
 --users
@@ -11,28 +17,42 @@ CREATE TABLE users (
 CREATE TABLE questions (
   id INTEGER PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
-  body VARCHAR(255) NOT NULL
+  body VARCHAR(255) NOT NULL,
+  author_id INTEGER FOREIGN KEY REFERENCES users(id)
 );
 
 --question_follows
 CREATE TABLE question_follows (
   id INTEGER PRIMARY KEY,
-  user_id INTEGER FOREIGN KEY,
-  question_id INTEGER FOREIGN KEY
+  user_id INTEGER FOREIGN KEY REFERENCES users(id),
+  question_id INTEGER FOREIGN KEY REFERENCES questions(id)
 );
 
 --replies
 CREATE TABLE replies (
   id INTEGER PRIMARY KEY,
-  question_id INTEGER FOREIGN KEY,
-  parent_reply_id INTEGER FOREIGN KEY,
-  author_id INTEGER FOREIGN KEY,
+  question_id INTEGER FOREIGN KEY REFERENCES questions(id),
+  parent_reply_id INTEGER FOREIGN KEY REFERENCES replies(id),
+  author_id INTEGER FOREIGN KEY REFERENCES users(id),
   body TEXT NOT NULL,
 );
 
 --question_likes
 CREATE TABLE question_likes (
   id INTEGER PRIMARY KEY,
-  user_id INTEGER FOREIGN KEY,
-  question_id INTEGER FOREIGN KEY
+  user_id INTEGER FOREIGN KEY REFERENCES users(id),
+  question_id INTEGER FOREIGN KEY REFERENCES questions(id)
 );
+
+INSERT INTO 
+  users (fname, lname)
+VALUES 
+  ("Jadin", "Press"), ("Sky", "Kumtong");
+
+INSERT INTO 
+  questions (title, body)
+VALUES 
+  ("adsfh;kjl", "asfhdladsh"), ("App", "Academy")
+
+
+
