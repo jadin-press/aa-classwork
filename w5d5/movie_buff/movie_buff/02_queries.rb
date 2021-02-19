188916b3fb1521bc946315f8b70b4313f1f4c18a
ibrahim-ali00 <ibrahim.ali00@protonmail.com>
2021-02-19 15:40:56 -0800

    01_specs

--- w5d5/movie_buff/movie_buff/02_queries.rb
+++ w5d5/movie_buff/movie_buff/02_queries.rb
@@ -3,6 +3,8 @@ def eighties_b_movies
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
 
+  Movie.select(:id, :title, :yr, :score).where(yr: 1980..1989, score: 3..5)
+
 end
 
 def bad_years
