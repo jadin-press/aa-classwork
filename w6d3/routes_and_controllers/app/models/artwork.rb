692ebf2676bb5bcee0a2ec3984995eb1be6f9b95
Suiki and Timothy <teriley138@gmail.com>
2021-02-25 14:45:38 -0800

    Make models and seed database

---
+++ w6d3/routes_and_controllers/app/models/artwork.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: artworks
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  image_url  :string           not null
+#  artist_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Artwork < ApplicationRecord
+     validates :title, :image_url, :artist_id, presence: true
+     validates :artist_id, uniqueness: {scope: :title} 
+
+     belongs_to :user,
+       foreign_key: :artist_id,
+       class_name: :User
+
+      has_many :artwork_shares,
+        foreign_key: :artwork_id,
+        class_name: :ArtworkShare
+
+      has_many :viewers,
+        through: :artwork_shares,
+        source: :viewer
+end
+      
--- w6d3/routes_and_controllers/db/seeds.rb
+++ w6d3/routes_and_controllers/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'Jim')
+User.create(username: 'Susan')
+User.create(username: 'Peter')
+User.create(username: 'Tim')
+User.create(username: 'Jadin')
+
+Artwork.create(title: 'Waterfall' , image_url: 'wf.com', artist_id: 1)
+Artwork.create(title: 'Rocks', image_url: 'rocks.com', artist_id: 2 )
+Artwork.create(title: 'Trees', image_url: 'Trees.com', artist_id: 3)
+Artwork.create(title: 'Birds', image_url: 'birdies.com', artist_id: 4)
+
+ArtworkShare.create(artwork_id: 1, viewer_id: 4)
+ArtworkShare.create(artwork_id:2 , viewer_id: 3)
+ArtworkShare.create(artwork_id: 4, viewer_id: 1)
+ArtworkShare.create(artwork_id: 2, viewer_id: 1)
+ArtworkShare.create(artwork_id: 1, viewer_id: 2)
