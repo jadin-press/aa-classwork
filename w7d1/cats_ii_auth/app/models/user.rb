3ab361e8330fdd7bbb6b6592fa0ce6391dc4ad17
jadin-press <jadinp@gmail.com>
2021-03-01 14:04:53 -0800

    15 minute switch

---
+++ w7d1/cats_ii_auth/app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  validates :user_name, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+
+  after_initialize :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
+end
