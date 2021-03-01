f2ec38e8d5a14dc7bda8bc2a7d3fc3a2f089d60d
tpadgett <tim_padgett@yahoo.com>
2021-03-01 14:40:49 -0800

    auth phase 2

--- w7d1/cats_ii_auth/app/models/user.rb
+++ w7d1/cats_ii_auth/app/models/user.rb
@@ -14,6 +14,24 @@ class User < ApplicationRecord
     self.session_token
   end
 
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+   BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(user_name, password)
+    user = User.find_by(user_name: user_name)
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
+  end
+
   private
   def ensure_session_token
     self.session_token ||= self.class.generate_session_token
--- w7d1/cats_ii_auth/db/migrate/20210301214744_make_changes.rb
+++ w7d1/cats_ii_auth/db/migrate/20210301214744_make_changes.rb
@@ -1,6 +1,12 @@
 class MakeChanges < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :session_token, :string, null: false
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :password_digest
+      t.string :session_token, null: false
+
+      t.timestamps
+    end
     add_index :users, :session_token, unique: true
   end
 end
