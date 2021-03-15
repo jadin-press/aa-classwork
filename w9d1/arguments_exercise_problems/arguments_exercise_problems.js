23cbf757d05738246c02bc7a4f9e6e9f6d837718
jadin-press <jadinp@gmail.com>
2021-03-15 16:02:14 -0700

    switch

--- w9d1/arguments_exercise_problems/arguments_exercise_problems.js
+++ w9d1/arguments_exercise_problems/arguments_exercise_problems.js
@@ -66,7 +66,42 @@ const curriedSum = function(numArgs) {
   return _curriedSum
 }
 
-const sum = curriedSum(5);
-console.log(sum(5)(30)(20)(1)(2)); // => 56
+// const sum = curriedSum(5);
+// console.log(sum(5)(30)(20)(1)(2)); // => 56
 
 
+Function.prototype.curry = function(numArgs) {
+  let args = [];
+  // let that = this;
+  const _curriedArgs = function(arg) {
+    args.push(arg);
+    if (args.length === numArgs) {
+      return that.apply(that, args);
+    } else {
+      return _curriedArgs;
+    };
+  };
+  return _curriedArgs;
+};
+
+function sumThree(num1, num2, num3, num4, num5) {
+  return num1 + num2 + num3 + num4 + num5;
+}
+
+// let f1 = sumThree.curry(3)
+// f1 = f1(4); // [Function]
+// f1 = f1(20); // [Function]
+// f1 = f1(6); // = 30
+// console.log(f1)
+console.log(sumThree.curry(5)(4)(21)(6)(5)(10))
+
+// console.log(sumThree(4, 20, 6)); // == 30
+
+// you'll write `Function#curry`!
+// let f1 = sumThree.curry(3); // tells `f1` to wait until 3 arguments are given before running `sumThree`
+// f1 = f1(4); // [Function]
+// f1 = f1(20); // [Function]
+// f1 = f1(6); // = 30
+
+// // or more briefly:
+// sumThree.curry(3)(4)(20)(6); // == 30
