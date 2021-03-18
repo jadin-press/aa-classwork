1a5104d1d287df7e287b24b03a45983a4f5cbd43
mike <michaeldeanmdmph@gmail.com>
2021-03-17 17:34:40 -0700

    next commit

--- w9d3/jquery_tic-tac-toe/src/ttt-view.js
+++ w9d3/jquery_tic-tac-toe/src/ttt-view.js
@@ -2,24 +2,61 @@ class View {
   constructor(game, $el) {
     this.game = game;
     this.$el = $el;
-
-  // bindEvents() {}
-
-  // makeMove($square) {}
-
-  this.setupBoard()
+  this.bindEvents();
+  this.setupBoard();
   }
 
+  bindEvents() {
+    this.$el.on("click", "li", ( event => {
+      const $square = $(event.currentTarget);
+      this.makeMove($square);
+    }));
 
+  }
+  makeMove($square) {
+    const pos = $square.data("pos");
+    const currentPlayer = this.game.currentPlayer;
+
+    try {
+      this.game.playMove(pos);
+    } catch (e) {
+      alert("This " + e.msg.toLowerCase());
+      return;
+    }
+
+    $square.addClass(currentPlayer);
+
+    if (this.game.isOver()) {
+      // cleanup click handlers.
+      this.$el.off("click");
+      this.$el.addClass("game-over");
+
+      const winner = this.game.winner();
+      const $figcaption = $("<figcaption>");
+
+      if (winner) {
+        this.$el.addClass(`winner-${winner}`);
+        $figcaption.html(`You win, ${winner}!`);
+      } else {
+        $figcaption.html("It's a draw!");
+      }
+
+      this.$el.append($figcaption);
+    }
+  }
 
   setupBoard() {
     let $ul = $("<ul>");
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < 3; i++) {
       let $li = $("<li>");
+      $li.data("pos", [i, 0]);
+      $li.data("pos", [i, 1]);
+      $li.data("pos", [i, 2]);
+
       $ul.append($li);
     };
     this.$el.append($ul);
-}
+  }
 
 
 }
