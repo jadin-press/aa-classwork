32920ff0e1ba015b3c67a0a6e796ec4cc26ed893
jadin-press <jadinp@gmail.com>
2021-04-01 14:32:50 -0700

    initial commit

---
+++ w11d4/bench_bnb/app/assets/javascripts/bundle.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./frontend/bench_bnb.jsx":
+/*!********************************!*\
+  !*** ./frontend/bench_bnb.jsx ***!
+  \********************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: Cannot find module 'babel-preset-env'\nRequire stack:\n- /Users/jadin/dev/app_academy/aa-classwork/w11d4/bench_bnb/node_modules/@babel/core/lib/config/files/plugins.js\n- /Users/jadin/dev/app_academy/aa-classwork/w11d4/bench_bnb/node_modules/@babel/core/lib/config/files/index.js\n- /Users/jadin/dev/app_academy/aa-classwork/w11d4/bench_bnb/node_modules/@babel/core/lib/index.js\n- /Users/jadin/dev/app_academy/aa-classwork/w11d4/bench_bnb/node_modules/babel-loader/lib/index.js\n- /Users/jadin/dev/app_academy/aa-classwork/w11d4/bench_bnb/node_modules/loader-runner/lib/loadLoader.js\n- /Users/jadin/dev/app_academy/aa-classwork/w11d4/bench_bnb/node_modules/loader-runner/lib/LoaderRunner.js\n- /Users/jadin/dev/app_academy/aa-classwork/w11d4/bench_bnb/node_modules/webpack/lib/NormalModule.js\n- /Users/jadin/dev/app_academy/aa-classwork/w11d4/bench_bnb/node_modules/webpack/lib/NormalModuleFactory.js\n- /Users/jadin/dev/app_academy/aa-classwork/w11d4/bench_bnb/node_modules/webpack/lib/Compiler.js\n- /Users/jadin/dev/app_academy/aa-classwork/w11d4/bench_bnb/node_modules/webpack/lib/webpack.js\n- /Users/jadin/dev/app_academy/aa-classwork/w11d4/bench_bnb/node_modules/webpack/lib/index.js\n- /Users/jadin/dev/app_academy/aa-classwork/w11d4/bench_bnb/node_modules/webpack-cli/lib/webpack-cli.js\n- /Users/jadin/dev/app_academy/aa-classwork/w11d4/bench_bnb/node_modules/webpack-cli/lib/bootstrap.js\n- /Users/jadin/dev/app_academy/aa-classwork/w11d4/bench_bnb/node_modules/webpack-cli/bin/cli.js\n- /Users/jadin/dev/app_academy/aa-classwork/w11d4/bench_bnb/node_modules/webpack/bin/webpack.js\n- Did you mean \"@babel/env\"?\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:880:15)\n    at resolve (/Users/jadin/dev/app_academy/aa-classwork/w11d4/bench_bnb/node_modules/v8-compile-cache/v8-compile-cache.js:164:23)\n    at resolveStandardizedName (/Users/jadin/dev/app_academy/aa-classwork/w11d4/bench_bnb/node_modules/@babel/core/lib/config/files/plugins.js:96:7)\n    at resolvePreset (/Users/jadin/dev/app_academy/aa-classwork/w11d4/bench_bnb/node_modules/@babel/core/lib/config/files/plugins.js:44:10)\n    at loadPreset (/Users/jadin/dev/app_academy/aa-classwork/w11d4/bench_bnb/node_modules/@babel/core/lib/config/files/plugins.js:63:20)\n    at loadPreset.next (<anonymous>)\n    at createDescriptor (/Users/jadin/dev/app_academy/aa-classwork/w11d4/bench_bnb/node_modules/@babel/core/lib/config/config-descriptors.js:179:16)\n    at createDescriptor.next (<anonymous>)\n    at step (/Users/jadin/dev/app_academy/aa-classwork/w11d4/bench_bnb/node_modules/gensync/index.js:261:32)\n    at evaluateAsync (/Users/jadin/dev/app_academy/aa-classwork/w11d4/bench_bnb/node_modules/gensync/index.js:291:5)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./frontend/bench_bnb.jsx"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
